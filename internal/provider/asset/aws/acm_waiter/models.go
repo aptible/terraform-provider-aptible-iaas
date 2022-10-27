35487ca689d2f06baf988c4999d755c0e5e347fe
Eric Bower <me@erock.io>
2022-10-27 09:09:40 -0400

    fixes

--- internal/provider/asset/aws/acm_waiter/models.go
+++ internal/provider/asset/aws/acm_waiter/models.go
@@ -3,12 +3,13 @@ package acmwaiter
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	cac "github.com/aptible/cloud-api-clients/clients/go"
 	"github.com/aptible/terraform-provider-aptible-iaas/internal/client"
-	"github.com/aptible/terraform-provider-aptible-iaas/internal/util"
+	assetutil "github.com/aptible/terraform-provider-aptible-iaas/internal/provider/asset/util"
 )
 
 var resourceTypeName = "_aws_acm_waiter"
@@ -23,7 +24,7 @@ type ResourceModel struct {
 	Status         types.String `tfsdk:"status" json:"status"`
 
 	CertificateArn  types.String `tfsdk:"certificate_arn"`
-	ValidationFqdns types.String `tfsdk:"validation_fqdns"`
+	ValidationFqdns types.List   `tfsdk:"validation_fqdns"`
 }
 
 var AssetSchema = map[string]tfsdk.Attribute{
@@ -49,15 +50,15 @@ var AssetSchema = map[string]tfsdk.Attribute{
 	},
 	"asset_version": {
 		Type:     types.StringType,
-		Required: true,
+		Computed: true,
 	},
-	"arn": {
+	"certificate_arn": {
 		Required: true,
 		Type:     types.StringType,
 	},
 	"validation_fqdns": {
 		Optional:    true,
-		Type:        types.StringType,
+		Type:        types.ListType{ElemType: types.StringType},
 		Description: "The DNS Records created to enable validation. This should include the validation records for both the primary domain and any SANs. Do not set if using EMAIL based validation.",
 	},
 }
@@ -67,13 +68,15 @@ func planToAssetInput(ctx context.Context, plan ResourceModel) (cac.AssetInput, 
 		"certificate_arn": plan.CertificateArn.Value,
 	}
 
-	if util.StringHasVal(plan.ValidationFqdns) {
-		params["validation_fqdns"] = plan.ValidationFqdns.Value
+	if !plan.ValidationFqdns.IsNull() && !plan.ValidationFqdns.IsUnknown() {
+		domains := []string{}
+		_ = plan.ValidationFqdns.ElementsAs(ctx, &domains, false)
+		params["validation_fqdns"] = domains
 	}
 
 	input := cac.AssetInput{
-		Asset:           client.CompileAsset("aws", "acm_certificate", plan.AssetVersion.Value),
-		AssetVersion:    plan.AssetVersion.Value,
+		Asset:           client.CompileAsset("aws", "acm_certificate", assetutil.DefaultAssetVersion),
+		AssetVersion:    assetutil.DefaultAssetVersion,
 		AssetParameters: params,
 	}
 
@@ -81,6 +84,15 @@ func planToAssetInput(ctx context.Context, plan ResourceModel) (cac.AssetInput, 
 }
 
 func assetOutputToPlan(ctx context.Context, plan ResourceModel, output *cac.AssetOutput) (*ResourceModel, error) {
+	domains := []attr.Value{}
+	for _, d := range output.CurrentAssetParameters.Data["validation_fqdns"].([]interface{}) {
+		domains = append(domains, types.String{Value: d.(string)})
+	}
+	validationFqdns := types.List{Elems: domains, ElemType: types.StringType}
+	if len(domains) == 0 {
+		validationFqdns.Null = true
+	}
+
 	model := &ResourceModel{
 		Id:              types.String{Value: output.Id},
 		AssetVersion:    types.String{Value: output.AssetVersion},
@@ -88,7 +100,7 @@ func assetOutputToPlan(ctx context.Context, plan ResourceModel, output *cac.Asse
 		OrganizationId:  types.String{Value: output.Environment.Organization.Id},
 		Status:          types.String{Value: string(output.Status)},
 		CertificateArn:  types.String{Value: output.CurrentAssetParameters.Data["certificate_arn"].(string)},
-		ValidationFqdns: util.StringVal(output.CurrentAssetParameters.Data["validation_fqdns"]),
+		ValidationFqdns: validationFqdns,
 	}
 
 	return model, nil
