ad5259dbb918362ec61862ac5a12fb737e50b9f3
Madhu <madhuravius@protonmail.com>
2022-11-09 14:17:43 -0500

    fix: added update_vpc and cleaned up create_vpc tests a little (#49)
    * fix: added update_vpc and cleaned up create_vpc tests a little
    
    * chore: dropped test case in demo from pair
    
    * fix: added create_vpc with reachability analysis and checks
    
    * fix: local conflict resolution
    
    * change asset bundle name
    
    * chore: revert rebase
    
    * chore: revert the reversion (mistake)
    
    * chore: reverted destroy step so there is a cleanup
    
    * fix: added reachability tests for update vpc commands as well
    
    * chore: ran go mod tidy
    
    * chore: changed package name, updated outputs
    
    * fix: addressed feedback from reviews
    
    Co-authored-by: Eric Bower <me@erock.io>

--- test/basic/vpc_create/vpc_create_test.go
+++ test/basic/vpc_create/vpc_create_test.go
@@ -1,4 +1,4 @@
-package vpc
+package vpc_create
 
 import (
 	"context"
@@ -15,7 +15,7 @@ import (
 	"github.com/aptible/terraform-provider-aptible-iaas/internal/client"
 )
 
-func TestRDS(t *testing.T) {
+func TestVPCCreate(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: ".",
 
@@ -23,10 +23,10 @@ func TestRDS(t *testing.T) {
 			"organization_id": os.Getenv("ORGANIZATION_ID"),
 			"environment_id":  os.Getenv("ENVIRONMENT_ID"),
 			"aptible_host":    os.Getenv("APTIBLE_HOST"),
-			"vpc_name":        "testrds-vpc",
+			"vpc_name":        "test-vpc",
 		},
 	})
-	//defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
 	c := client.NewClient(
@@ -51,10 +51,33 @@ func TestRDS(t *testing.T) {
 	vpcAws, vpcAwsErr := terratest_aws.GetVpcsE(t, []*legacy_aws_sdk_ec2.Filter{
 		{
 			Name:   aws.String("tag:Name"),
-			Values: []*string{aws.String("testrds-vpc")},
+			Values: []*string{aws.String("test-vpc")},
 		},
 	}, "us-east-1")
 	assert.Nil(t, vpcAwsErr)
 	assert.GreaterOrEqual(t, len(vpcAws), 1)
 	assert.Equal(t, len(vpcAws[0].Subnets), 6)
+	assert.Equal(t, vpcAws[0].Name, "test-vpc")
+
+	privInstanceENI := terraform.Output(t, terraformOptions, "test_instance_private_eni")
+	pubInstanceENI := terraform.Output(t, terraformOptions, "test_instance_public_eni")
+	analysisId := terraform.Output(t, terraformOptions, "analysis_id")
+	insightsId := terraform.Output(t, terraformOptions, "insights_id")
+
+	ec2 := terratest_aws.NewEc2Client(t, "us-east-1")
+	networkInsights, networkInsightsErr := ec2.DescribeNetworkInsightsPaths(&legacy_aws_sdk_ec2.DescribeNetworkInsightsPathsInput{
+		NetworkInsightsPathIds: []*string{aws.String(insightsId)},
+	})
+	assert.Nil(t, networkInsightsErr)
+	assert.Equal(t, len(networkInsights.NetworkInsightsPaths), 1)
+	assert.Equal(t, *networkInsights.NetworkInsightsPaths[0].Destination, pubInstanceENI)
+	assert.Equal(t, *networkInsights.NetworkInsightsPaths[0].Source, privInstanceENI)
+
+	networkAnalysis, networkAnalysisErr := ec2.DescribeNetworkInsightsAnalyses(
+		&legacy_aws_sdk_ec2.DescribeNetworkInsightsAnalysesInput{
+			NetworkInsightsAnalysisIds: []*string{aws.String(analysisId)},
+		})
+	assert.Nil(t, networkAnalysisErr)
+	assert.Equal(t, len(networkAnalysis.NetworkInsightsAnalyses), 1)
+	assert.Equal(t, *networkAnalysis.NetworkInsightsAnalyses[0].Status, "succeeded")
 }
