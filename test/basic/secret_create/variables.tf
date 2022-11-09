f861fa19ef593c16e636109fe137ab41ac43d684
Madhu <madhuravius@protonmail.com>
2022-11-09 10:08:17 -0500

    fix: added secrets manager add flow (#47)
    * fix: added secrets manager add flow
    
    * chore: added another test (mostly redundant)

---
+++ test/basic/secret_create/variables.tf
@@ -0,0 +1,19 @@
+variable "organization_id" {
+  type = string
+}
+
+variable "environment_id" {
+  type = string
+}
+
+variable "aptible_host" {
+  type = string
+}
+
+variable "secret_name" {
+  type = string
+}
+
+variable "secret_value" {
+  type = string
+}
