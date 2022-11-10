47eba4e77e7b538835b00f263ee9eacf88cb3731
Robert Hafner <tedivm@tedivm.com>
2022-11-10 10:03:01 -0500

    Add Elasticache Create Tests

---
+++ test/basic/elasticache/elasticache_test.go
@@ -0,0 +1,159 @@
+package elasticache
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache/types"
+
+	//"github.com/aws/aws-sdk-go/aws/awserr"
+	//"github.com/aws/aws-sdk-go/aws/session"
+	legacy_aws_sdk_ec2 "github.com/aws/aws-sdk-go/service/ec2"
+
+	terratest_aws "github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+
+	cac "github.com/aptible/cloud-api-clients/clients/go"
+	"github.com/aptible/terraform-provider-aptible-iaas/internal/client"
+)
+
+func cleanupAndAssert(t *testing.T, terraformOptions *terraform.Options) {
+	terraform.Destroy(t, terraformOptions)
+
+	// test / assert all failures here
+}
+
+func TestElasticache(t *testing.T) {
+	vpc_name := "test-elasticache-vpc"
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: ".",
+
+		Vars: map[string]interface{}{
+			"organization_id":    os.Getenv("ORGANIZATION_ID"),
+			"environment_id":     os.Getenv("ENVIRONMENT_ID"),
+			"aptible_host":       os.Getenv("APTIBLE_HOST"),
+			"node_name":          "test-elasticache-node",
+			"vpc_name":           vpc_name,
+			"description":        "TestingModule",
+			"snapshot_window":    "01:00-04:00",
+			"maintenance_window": "sun:05:00-sun:09:00",
+		},
+	})
+
+	// Make sure to delete when the function returns.
+	defer cleanupAndAssert(t, terraformOptions)
+
+	// Run the Terraform Workspace.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Initialize the Cloud-API Client
+	c := client.NewClient(
+		true,
+		os.Getenv("APTIBLE_HOST"),
+		os.Getenv("APTIBLE_TOKEN"),
+	)
+	ctx := context.Background()
+
+	// Get VPC Asset ID from terraform output and use it to get the Asset from Cloud-API
+	vpcId := terraform.Output(t, terraformOptions, "vpc_id")
+
+	// check cloud api's understanding of asset
+	vpcAsset, vpcAptibleErr := c.DescribeAsset(
+		ctx,
+		os.Getenv("ORGANIZATION_ID"),
+		os.Getenv("ENVIRONMENT_ID"),
+		vpcId,
+	)
+	assert.Nil(t, vpcAptibleErr)
+	assert.Equal(t, vpcAsset.Id, vpcId)
+	assert.Equal(t, vpcAsset.Status, cac.ASSETSTATUS_DEPLOYED)
+
+	// check aws asset state
+	vpcAws, vpcAwsErr := terratest_aws.GetVpcsE(t, []*legacy_aws_sdk_ec2.Filter{
+		{
+			Name:   aws.String("tag:Name"),
+			Values: []*string{aws.String(vpc_name)},
+		},
+	}, "us-east-1")
+	assert.Nil(t, vpcAwsErr)
+	assert.GreaterOrEqual(t, len(vpcAws), 1)
+	assert.Equal(t, len(vpcAws[0].Subnets), 6)
+
+	redisId := terraform.Output(t, terraformOptions, "redis_id")
+
+	// check cloud api's understanding of asset
+	redisAsset, redisErr := c.DescribeAsset(
+		ctx,
+		os.Getenv("ORGANIZATION_ID"),
+		os.Getenv("ENVIRONMENT_ID"),
+		redisId,
+	)
+	assert.Nil(t, redisErr)
+	assert.Equal(t, redisAsset.Id, redisId)
+	assert.Equal(t, redisAsset.Status, cac.ASSETSTATUS_DEPLOYED)
+	assert.NotNil(t, redisAsset.Outputs)
+
+	redis_cluster_id_base := redisAsset.GetOutputs()["elasticache_cluster_id"].Data.(string)
+
+	cluster_details, err := GetCluster(redis_cluster_id_base + "-001")
+	assert.Nil(t, err)
+	assert.Equal(t, *cluster_details.CacheClusterStatus, "available")
+}
+
+func GetCluster(cluster_id string) (*types.CacheCluster, error) {
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	c := elasticache.NewFromConfig(cfg)
+	clusters, err := c.DescribeCacheClusters(ctx, &elasticache.DescribeCacheClustersInput{CacheClusterId: aws.String(cluster_id)})
+	if err != nil {
+		return nil, err
+	}
+	if clusters == nil || len(clusters.CacheClusters) == 0 {
+		return nil, fmt.Errorf("aws returned nil object or no clusters from search parameters")
+	}
+
+	return &clusters.CacheClusters[0], nil
+}
+
+// func GetCluster(cluster_id string) (elasticache.CacheCluster, error) {
+
+// 	svc := elasticache.New(session.New())
+// 	input := &elasticache.DescribeCacheClustersInput{
+// 		CacheClusterId: aws.String(cluster_id),
+// 	}
+
+// 	result, err := svc.DescribeCacheClusters(input)
+// 	if err != nil {
+// 		if aerr, ok := err.(awserr.Error); ok {
+// 			switch aerr.Code() {
+// 			case elasticache.ErrCodeCacheClusterNotFoundFault:
+// 				return elasticache.CacheCluster{}, fmt.Errorf("%s - %s", elasticache.ErrCodeCacheClusterNotFoundFault, aerr.Error())
+// 			case elasticache.ErrCodeInvalidParameterValueException:
+// 				return elasticache.CacheCluster{}, fmt.Errorf("%s - %s", elasticache.ErrCodeInvalidParameterValueException, aerr.Error())
+// 			case elasticache.ErrCodeInvalidParameterCombinationException:
+// 				return elasticache.CacheCluster{}, fmt.Errorf("%s - %s", elasticache.ErrCodeInvalidParameterCombinationException, aerr.Error())
+// 			default:
+// 				return elasticache.CacheCluster{}, fmt.Errorf(aerr.Error())
+// 			}
+// 		} else {
+// 			// Print the error, cast err to awserr.Error to get the Code and
+// 			// Message from an error.
+// 			fmt.Println(err.Error())
+// 		}
+// 		return elasticache.CacheCluster{}, err
+// 	}
+
+// 	fmt.Println(result)
+// 	return *result.CacheClusters[0], nil
+// }
