f35cd7535f1bff17525edc914a84067882e6b4fa
Robert Hafner <tedivm@tedivm.com>
2022-11-10 16:18:42 -0500

    Add test for elasticache update

---
+++ test/basic/elasticache_update/variables.tf
@@ -0,0 +1,31 @@
+variable "organization_id" {
+  type = string
+}
+
+variable "environment_id" {
+  type = string
+}
+
+variable "aptible_host" {
+  type = string
+}
+
+variable "node_name" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "snapshot_window" {
+  type = string
+}
+
+variable "maintenance_window" {
+  type = string
+}
