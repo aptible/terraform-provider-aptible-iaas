c5bee43ce9b51aeba669898331583d8a03fb79c7
Ann Guilinger <ann.guilinger@aptible.com>
2022-11-18 15:49:43 -0500

    ECS Web update tests (#57)
    * Update with secrets
    
    * More update tests
    
    * linting
    
    * Woopsies

---
+++ test/basic/ecs_web_update/main.tf
@@ -0,0 +1,112 @@
+terraform {
+  required_providers {
+    aptible = {
+      source = "aptible.com/aptible/aptible-iaas"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "dns_account"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.dns_account_id}:role/OrganizationAccountAccessRole"
+  }
+}
+
+provider "aptible" {
+  host = var.aptible_host
+}
+
+data "aptible_organization" "org" {
+  id = var.organization_id
+}
+
+data "aptible_environment" "env" {
+  id     = var.environment_id
+  org_id = data.aptible_organization.org.id
+}
+
+resource "aptible_aws_vpc" "vpc" {
+  environment_id  = data.aptible_environment.env.id
+  organization_id = data.aptible_organization.org.id
+  name            = var.vpc_name
+}
+
+resource "aptible_aws_acm" "cert" {
+  environment_id  = data.aptible_environment.env.id
+  organization_id = data.aptible_organization.org.id
+
+  fqdn = "${var.subdomain}.${var.domain}"
+
+  validation_method = "DNS"
+}
+
+locals {
+  validation_dns = [
+    for record in aptible_aws_acm.cert.domain_validation_records : {
+      name   = record.resource_record_name
+      record = record.resource_record_value
+      type   = record.resource_record_type
+    }
+  ]
+}
+
+data "aws_route53_zone" "domains" {
+  name         = var.domain
+  private_zone = false
+  provider     = aws.dns_account
+}
+
+resource "aws_route53_record" "domains" {
+  allow_overwrite = true
+  name            = local.validation_dns.0.name
+  records         = [local.validation_dns.0.record]
+  ttl             = 60
+  type            = local.validation_dns.0.type
+  zone_id         = data.aws_route53_zone.domains.zone_id
+  depends_on      = [aptible_aws_acm.cert]
+  provider        = aws.dns_account
+}
+
+resource "aptible_aws_acm_waiter" "waiter" {
+  environment_id   = data.aptible_environment.env.id
+  organization_id  = data.aptible_organization.org.id
+  certificate_arn  = aptible_aws_acm.cert.arn
+  validation_fqdns = [for dns in local.validation_dns : dns.name]
+}
+
+
+resource "aptible_aws_ecs_web" "web" {
+  environment_id  = data.aptible_environment.env.id
+  organization_id = data.aptible_organization.org.id
+  vpc_name        = aptible_aws_vpc.vpc.name
+  depends_on      = [aptible_aws_acm_waiter.waiter]
+
+  name                  = var.ecs_name
+  container_name        = var.container_name
+  container_image       = var.container_image
+  wait_for_steady_state = true
+
+  container_registry_secret_arn = var.registry_credentials_arn
+  container_command             = var.container_command
+  container_port                = var.container_port
+  is_public                     = var.is_public
+  is_ecr_image                  = var.is_ecr_image
+  lb_cert_arn                   = aptible_aws_acm.cert.arn
+  lb_cert_domain                = aptible_aws_acm.cert.fqdn
+  environment_secrets           = var.environment_secrets
+}
+
+resource "aws_route53_record" "cname" {
+  allow_overwrite = true
+  name            = aptible_aws_acm.cert.fqdn
+  records         = [aptible_aws_ecs_web.web.load_balancer_url]
+  ttl             = 60
+  type            = "CNAME"
+  zone_id         = data.aws_route53_zone.domains.zone_id
+  provider        = aws.dns_account
+}
---
+++ test/basic/ecs_web_update/registry_secrets/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aptible = {
+      source = "aptible.com/aptible/aptible-iaas"
+    }
+  }
+}
+
+variable "organization_id" {
+  type = string
+}
+
+variable "environment_id" {
+  type = string
+}
+
+variable "aptible_host" {
+  type = string
+}
+
+variable "registry_secret_name" {
+  type = string
+}
+
+variable "registry_secret_username" {
+  type = string
+}
+variable "registry_secret_password" {
+  type = string
+}
+
+provider "aptible" {
+  host = var.aptible_host
+}
+
+data "aptible_organization" "org" {
+  id = var.organization_id
+}
+
+data "aptible_environment" "env" {
+  id     = var.environment_id
+  org_id = data.aptible_organization.org.id
+}
+
+resource "aptible_aws_secret" "registry_secret" {
+  environment_id  = data.aptible_environment.env.id
+  organization_id = data.aptible_organization.org.id
+  name            = var.registry_secret_name
+  secret_string = jsonencode({
+    username = var.registry_secret_username
+    password = var.registry_secret_password
+  })
+}
+
+output "registry_secret_id" {
+  value = aptible_aws_secret.registry_secret.id
+}
+
+output "registry_secret_arn" {
+  value = aptible_aws_secret.registry_secret.arn
+}
